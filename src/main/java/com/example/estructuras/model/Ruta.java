663a5772ee240e3585a481553a49b297c09c1853
IsabelaOspina <isabela.ospinab@uqvirtual.edu.co>
2025-11-16 18:22:13 -0500

    fix: se corrigio la clase ruta del modelo

--- src/main/java/com/example/estructuras/model/Ruta.java
+++ src/main/java/com/example/estructuras/model/Ruta.java
@@ -10,44 +10,23 @@ public class Ruta {
         this.destino = destino;
         this.distancia = distancia;
     }
-
     public Ruta() {}
-
-    public int calcularPeso(){
-        int peso = 0;
-        int comparador = 5;
-        if (distancia <= 0) return peso;
-        for(int i = 1; i <= 10; i++){
-            if(distancia >= comparador){
-                peso = i;
-                comparador += 5;
-            }else{
-                break;
-            }
-        }
-        return peso;
-    }
 
     public Ubicacion getOrigen() {
         return origen;
     }
-
     public void setOrigen(Ubicacion origen) {
         this.origen = origen;
     }
-
     public Ubicacion getDestino() {
         return destino;
     }
-
     public void setDestino(Ubicacion destino) {
         this.destino = destino;
     }
-
     public float getDistancia() {
         return distancia;
     }
-
     public void setDistancia(int distancia) {
         this.distancia = distancia;
     }
