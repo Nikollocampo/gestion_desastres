7b99de2f1f7000aea608ea9c111e0ea1600c81b0
IsabelaOspina <isabela.ospinab@uqvirtual.edu.co>
2025-11-17 09:22:59 -0500

    feat: se creo informacion en el json

--- src/main/java/com/example/estructuras/controller/UbicacionController.java
+++ src/main/java/com/example/estructuras/controller/UbicacionController.java
@@ -25,22 +25,22 @@ public class UbicacionController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @GetMapping
+    @GetMapping("/listar")
     public ResponseEntity<List<UbicacionResponseDto>> listar() throws IOException {
         return ResponseEntity.ok(ubicacionService.listar());
     }
 
-    @GetMapping("/id")
+    @GetMapping("/{id}")
     public ResponseEntity<UbicacionResponseDto> obtenerPorId(@PathVariable String id) throws IOException {
         return ResponseEntity.ok(ubicacionService.obtenerPorId(id));
     }
 
-    @PutMapping("/id")
+    @PutMapping("/{id}")
     public ResponseEntity<UbicacionResponseDto> actualizar(@PathVariable String id, @RequestBody UbicacionRequestDto dto) throws IOException {
         return ResponseEntity.ok(ubicacionService.actualizar(id, dto));
     }
 
-    @DeleteMapping("/id")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminar(@PathVariable String id) throws IOException {
         ubicacionService.eliminar(id);
         return ResponseEntity.noContent().build();
